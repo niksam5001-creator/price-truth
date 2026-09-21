574f944ee41de700d77089c8e11009eb7b4b0182
Nikolay <nikolay@studioforge.local>
2026-09-21 09:59:12 +0300

    fix pwa manifest and sw

--- sw.js
+++ sw.js
@@ -1,14 +1,10 @@
-const CACHE = 'pt-v1';
-const ASSETS = ['/', '/index.html', '/app.js', '/manifest.webmanifest', '/icon-192.png', '/icon-512.png'];
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-});
+const CACHE = 'pt-v2';
+const ASSETS = ['./', 'index.html', 'app.js', 'manifest.webmanifest', 'icon-192.png', 'icon-512.png'];
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(hit => hit || fetch(e.request).then(r => {
-      const copy = r.clone();
-      caches.open(CACHE).then(c => c.put(e.request, copy));
-      return r;
-    }).catch(() => caches.match('/index.html')))
-  );
+  e.respondWith(caches.match(e.request).then(hit => hit || fetch(e.request).then(r => {
+    const copy = r.clone();
+    caches.open(CACHE).then(c => c.put(e.request, copy));
+    return r;
+  }).catch(() => caches.match('index.html'))));
 });
